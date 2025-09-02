a3e7a85412260aba0e4af8fa1032b82662b8e933
Mostafa Galal <116258305+MG-B17@users.noreply.github.com>
2025-09-02 16:01:36 +0300

    first flutter update

---
+++ Flutter/anomatrix/lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:anomatrix/core/utils/color/color_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData appTheme =ThemeData(
+  primaryColor: ColorManager.primaryColor,
+  scaffoldBackgroundColor: ColorManager.primaryColor,
+  fontFamily: "Geist"
+);
---
+++ Flutter/anomatrix/lib/core/utils/color/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class ColorManager {
+  static const Color primaryColor = Color.fromRGBO(5, 8,31, 1);
+
+  static const Color secondaryColor = Color.fromRGBO(58, 103, 139, 1);
+
+  static const Color textColor = Colors.white;
+}
