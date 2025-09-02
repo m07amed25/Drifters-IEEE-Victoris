b0e46a85be633af0081f353face026391bf4d429
hanaa <113058547+hanaa-nabil@users.noreply.github.com>
2025-09-02 15:37:06 +0300

    Delete Utilities directory

--- Utilities/SD.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Utilities
-{
-    public static class SD
-    {
-        public const string AdminRole = "Admin";
-        public const string ManagerRole = "Manager";
-        public const string ObserverRole = "Observer";
-    }
-}
